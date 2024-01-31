38cdd2ab7d2bbf5376829dbcd06531e08ca306d0
emilianogastaldo <emiliano0303@gmail.com>
2024-01-31 15:44:57 +0100

    added coments and css

--- src/components/ViteHello.vue
+++ src/components/ViteHello.vue
@@ -1,11 +1,22 @@
 <script>
+// Qui metto la logica del componenete
 export default {
     name: 'ViteHello'
 }
 </script>
 
 <template>
-    <h1>CIAO</h1>
+    <!-- Qui metto la struttura html del componente -->
+    <main>
+        <h1>Ciao Mondo!</h1>
+    </main>
 </template>
 
-<style></style>
+<style scoped>
+/* Qui metto lo stile specifico del componente */
+h1 {
+    color: blueviolet;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+}
+</style>
