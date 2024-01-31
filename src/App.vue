5af11711bdfedc59b68db78d84e44d8cf5da34f8
emilianogastaldo <emiliano0303@gmail.com>
2024-01-31 15:37:29 +0100

    formatto il file main

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import ViteHello from './components/ViteHello.vue'
+
+export default {
+  name: 'myRoot',
+  data: () => ({
+    // possibili dati
+  }),
+  computed: {
+    // possibili computed
+  },
+  methods: {
+    // possibili metodi
+  },
+  components: {
+    ViteHello
+  }
+  // eccetera
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- Qui metto la struttura generale della pagina -->
+  <ViteHello />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+/* Qui metto lo stile generale */
 </style>
