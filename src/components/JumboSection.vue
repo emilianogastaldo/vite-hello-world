9430230524a8c306f9f9f35c6543618a2b58426d
emilianogastaldo <emiliano0303@gmail.com>
2024-01-31 15:53:45 +0100

    created jumbo section

---
+++ src/components/JumboSection.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'JumboSection'
+}
+</script>
+
+<template>
+    <section>
+        <h2>SEZIONE JUMBOTRON</h2>
+        <figure>
+            <img src="../assets/jumbo.png" alt="jumbo">
+        </figure>
+    </section>
+</template>
+
+<style scoped>
+section {
+    border: 2px dotted brown;
+    border-radius: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 2px 2px 2px white;
+}
+</style>
